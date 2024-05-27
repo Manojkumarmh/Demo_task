0ea022628dbbaf448626d9b1bfbf96ddc8951297
Manojkumarmh <160715812+Manojkumarmh@users.noreply.github.com>
2024-05-27 20:02:31 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,15 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
+
 variable "aws_region" {
   default = "us-east-1"
 }
